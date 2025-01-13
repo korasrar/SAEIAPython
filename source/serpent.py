883b654e287cf9001dcd1735fb1cb35e4fe1c17c
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-01-13 10:12:25 +0100

    to str

--- source/serpent.py
+++ source/serpent.py
@@ -154,7 +154,7 @@ def to_str(serpent:dict)->str:
     Returns:
         str: la chaine de caractères donnant les informations principales d'un serpent 
     """    
-    return f"serpent["nom_joueur"] -> serpent[""]"
+    return serpent["nom_joueur"] ,"->", serpent["points"], "s:",serpent["tps_s"], "m:",serpent["tps_m"], "p:",serpent["tps_m"]
 
 def get_temps_protection(serpent:dict)->int:
     """indique le temps restant pour le bonus protection
