96f9176766adb0b6085629da0845df03b3ad1907
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-01-13 12:10:26 +0100

    ar

--- source/test_serpent.py
+++ source/test_serpent.py
@@ -1,28 +1,28 @@
-dico_test : dict = {"nom_joueur": "morad", "num_joueur":1, "points":3, "positions":positions, 
-"tps_s":tps_s, "tps_p":tps_p, "tps_m":tps_m, "direction":direction}
+import serpent as s
 
+dico_test : dict = {"nom_joueur": "morad", "num_joueur":1, "points":3, "positions":[[0,0],[1,0],[1,1]]
+,"tps_s":3, "tps_p":3, "tps_m":3, "direction":'N'}
 
 def test_get_nom():
-    assert get_nom(dico_serpent) == "test1"
+    assert s.get_nom(dico_test) == "test1"
     
 def test_get_num_joueur():
-    assert get_num_joueur(dico_serpent) == 1
+    assert s.get_num_joueur(dico_test) == 1
 
 def test_get_points():
-    assert get_points(dico_serpent) == 0
+    assert s.get_points(dico_test) == 0
     
 def test_get_liste_pos():
-    assert get_liste_pos(dico_serpent) == ...
+    assert s.get_liste_pos(dico_test) == ...
 
 def test_get_queue():
-    assert get_queue(dico_serpent) == ...
-    
+    assert s.get_queue(dico_test) == ...
     
 def test_get_derniere_direction():
-    assert get_derniere_direction(dico_serpent) == 'N'
+    assert s.get_derniere_direction(dico_test) == 'N'
 
 def test_get_bonus():
-    assert get_bonus(dico_serpent) == ...
+    assert s.get_bonus(dico_test) == ...
     
 def test_ajouter_points():
-    assert ajouter_points(dico_serpent, 5) == 5
+    assert s.ajouter_points(dico_test, 5) == 8
